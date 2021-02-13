1b5768c0f28262abb3fc88b49daffd30576d46da
Robert Tilton <robtilton2086@gmail.com>
2021-02-13 14:27:25 -0500

    removed function to determine timing as it was ruining the rest of my moment.js code

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ var userInput = document.querySelectorAll(".inputField");
 var currentTime = document.getElementById("currentTime");
 var inputTime = document.querySelectorAll(".time");
 var rows = document.querySelectorAll(".row");
+
 // time and date moment.js implementation
+
 function timedateUpdate() {
     currentDate.textContent = moment().format("MMM Do, YYYY");
     currentTime.textContent = moment().format("h a");
@@ -18,19 +20,11 @@ for (var i = 0; i < confirmButton.length; i++) {
         // save text to localStorage 
         localStorage.setItem("userInput", currentText);
     })
-}
+};
 
 // get info from local storage and put it in the textArea
 var local = localStorage.getItem(userInput);
 userInput.textContent = local;
-
-if ((moment().format("h")) === inputTime) {
-    rows.style.backgroundColor = red;
-} else if (moment().format("h") > inputTime) {
-    rows.style.backgroundColor = green;
-} else {
-    rows.style.backgroundColor = black;
-}
 
 // some leftover pieces of the time and date function, mostly about having it run every second
 timedateUpdate();
