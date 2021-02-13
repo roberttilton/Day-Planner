ebaa7f1fe02223a662f8ffc5fdb3bb55f0268022
Robert Tilton <robtilton2086@gmail.com>
2021-02-13 13:07:07 -0500

    updated and pushing after tutor session, completely overhauled html and a lot of the javascript as well as added pseudocode to continue

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 var currentDate = document.getElementById("currentDay");
-
+//var timeDivs = document.getElementById("timeSelect").querySelectorAll("div")
+var confirmButton = document.querySelectorAll(".confirmBtn")
+var userInput = document.querySelectorAll(".inputField")
+var currentTime;
 function timedateUpdate() {
     currentDate.textContent = moment().format("MMM Do, YYYY" + " " + "hh:mm:ss");
+    currentTime = moment().format()
 };
+
+for (var i = 0; i < confirmButton.length; i++) {
+    confirmButton[i].addEventListener("click", function () {
+        var currentText = console.log(this.parentNode.parentNode.children[1].querySelector(".inputField").value)
+
+
+        // save text to localStorage 
+
+    })
+}
+
+
+// get info from local storage and put it in the textArea
+
+
+// function dateColor () {
+//     if (timeDivs = timedateUpdate()) {
+//         timeDivs.
+//     }
+// }
 
 timedateUpdate();
 setInterval(function(){
